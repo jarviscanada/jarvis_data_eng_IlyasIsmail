e4f598cf84ae8bcc553e3442eaf5da04387a37e1
Ilyas Ismail <m.ilyasismaiil@gmail.com>
2024-08-14 20:08:18 +0000

    Implemented basic DAO

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,7 +1,24 @@
 package ca.jrvs.apps.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
     public static void main(String[] args) {
-        System.out.println("Hello Learning JDBC");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "pass");
+        try {
+            Connection connection = dcm.getConnection();
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            Customer customer = customerDAO.findById(10000);
+            System.out.println(customer.toString());
+            customer.setFirstName("Example");
+            customer = customerDAO.update(customer);
+            System.out.println(customer.toString());
+            customerDAO.delete(customer.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
