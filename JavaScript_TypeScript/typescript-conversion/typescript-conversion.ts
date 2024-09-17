197604c13b5d9bf3b496f0290f863672f0438cb2
Ilyas Ismail <m.ilyasismaiil@gmail.com>
2024-09-17 19:25:49 +0000

    Converted JS practice to TS

---
+++ JavaScript_TypeScript/typescript-conversion/typescript-conversion.ts
@@ -0,0 +1,84 @@
+function driver() {
+    analyzeContent("This\nIs\nA\nLot\nOf\nNew\nLines!");
+    analyzeContent("body{this is a html body} a{color:#fef0} a{padding:100} c{color:#fff}");
+    analyzeContent("<html><head></head></html><div><p>Lots of text</p></div><p>Closing text</p><html></html>");
+}
+
+function textObj(str: string): { contentType: string; lineNumber: number} {
+    let temp = str;
+    let contentType = "TEXT";
+    let lineNumber = 1;
+
+    while (temp.indexOf("\n") != -1) {
+        temp = temp.substring(0, temp.lastIndexOf("\n"));
+        lineNumber += 1;
+    }
+
+    const text: { contentType: string; lineNumber: number } = {contentType, lineNumber};
+    return text;
+}
+
+
+function cssObj(words: string[]): { contentType: string; cssTargets: Record<string, number>} {
+    let i = 0;
+    for (const element of words) {
+        let temp = element.substring(0, element.indexOf("{"));
+        words[i] = temp;
+        i += 1;
+    }
+
+    let contentType = "CSS";
+    let cssTargets = findRecurring(words);
+
+    const css: { contentType: string; cssTargets: Record<string, number>} = {contentType, cssTargets};
+
+    return css;
+}
+
+function htmlObj(words: string[]): { contentType: string; tags: Record<string, number> } {
+    let i = 0;
+    for (const element of words) {
+        let temp = element.substring(element.indexOf("<") + 1, element.indexOf(">"));
+        words[i] = temp;
+        i += 1;
+    }
+
+    let contentType = "HTML";
+    let tags = findRecurring(words);
+
+    const html: { contentType: string; tags: Record<string, number> } = {contentType, tags};
+
+    return html;
+}
+
+function findRecurring(words: string[]): Record<string, number> {
+    let recurring: Record<string, number> = {};
+
+    for (const word of words) {
+        if (recurring[word]) {
+            recurring[word]++;
+        } else {
+            recurring[word] = 1;
+        }
+    }
+
+    return recurring;
+}
+
+function analyzeContent(str: string) {
+    let cssPattern = /[a-zA-Z0-9_]+{[^}]*}/g
+    let htmlPattern = /<[a-zA-Z0-9_]*>/g
+
+    let words;
+
+    if (words = str.match(cssPattern)) {
+        console.log(cssObj(words));
+    } else if (words = str.match(htmlPattern)) {
+        console.log(htmlObj(words));
+    } else {
+        console.log(textObj(str));
+    }
+}
+
+driver();
+
